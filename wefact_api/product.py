fde19e173f1212daf3fb752734c48b939856c3f8
Richard Kooijman <kooijman.richard@gmail.com>
2026-07-13 21:53:02 +0200

    resolved merge conflict error (#6)

--- wefact_api/product.py
+++ wefact_api/product.py
@@ -10,22 +10,42 @@ def product_data_id_from_model(line_item: LineItem):
 
 
 def product_data_add(code, name, key_phrase, price, cost_center):
-    return {"ProductCode": code, "ProductName": name, "ProductKeyPhrase": key_phrase, "PriceExcl": price, "AccountingCostCentre": cost_center}
+    return {
+        "ProductCode": code,
+        "ProductName": name,
+        "ProductKeyPhrase": key_phrase,
+        "PriceExcl": price,
+        "AccountingCostCentre": cost_center,
+    }
 
 
 def product_data_edit(id, code, name, key_phrase, price, cost_center):
-    return {"Identifier": id, "ProductCode": code, "ProductName": name, "ProductKeyPhrase": key_phrase,
-            "PriceExcl": price, "AccountingCostCentre": cost_center}
-
-
-def product_data_edit(id, code, name, key_phrase, price):
-    return {"Identifier": id, "ProductCode": code, "ProductName": name, "ProductKeyPhrase": key_phrase,
-            "PriceExcl": price}
+    return {
+        "Identifier": id,
+        "ProductCode": code,
+        "ProductName": name,
+        "ProductKeyPhrase": key_phrase,
+        "PriceExcl": price,
+        "AccountingCostCentre": cost_center,
+    }
 
 
 def product_data_add_from_model(line_item: LineItem):
-    return product_data_add(line_item.hs_sku, line_item.name, line_item.name, line_item.price, line_item.kostenplaats)
+    return product_data_add(
+        line_item.hs_sku,
+        line_item.name,
+        line_item.name,
+        line_item.price,
+        line_item.kostenplaats,
+    )
 
 
 def product_data_edit_from_model(id, line_item: LineItem):
-    return product_data_edit(id, line_item.hs_sku, line_item.name, line_item.name, line_item.price, line_item.kostenplaats)
+    return product_data_edit(
+        id,
+        line_item.hs_sku,
+        line_item.name,
+        line_item.name,
+        line_item.price,
+        line_item.kostenplaats,
+    )
